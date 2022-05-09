7653b8f1894e0719d6238c9c7a2db7b178d63d42
dlicari <38352645+dlicari@users.noreply.github.com>
2022-05-09 08:41:18 +0200

    M1L06

--- jupyter/jupyterNotebooks/otherpath/test_import_py_Copy1.py
+++ jupyter/jupyterNotebooks/otherpath/test_import_py_Copy1.py
@@ -1,4 +1,14 @@
 def my_function_in_otherpath():
+    """
+    It prints 'Successfully imported from otherpath!' string
+    
+    :param: no parameter is required
+    
+    :return: no return value, it is a void function 
+    --- 
+    Usage:
+    > my_function_in_otherpath()
+    
+    """
     print('Successfully imported from otherpath!')
 
-#my_function()
